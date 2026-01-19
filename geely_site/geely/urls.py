b976317987102dd6fa408bd0432b62e27fce3a65
rdnff11 <166251630+rdnff11@users.noreply.github.com>
2026-01-19 22:42:12 +0300

    Update urls.py

--- geely_site/geely/urls.py
+++ geely_site/geely/urls.py
@@ -6,4 +6,6 @@ app_name = 'geely'
 urlpatterns = [
     path('', views.CategoryListView.as_view(), name='cat_list'),
     path('<slug:slug>/', views.AddExpense.as_view(), name='cat_detail'),
+    path('<slug:slug>/exam/', views.ExamExpense.as_view(), name='exam'),
+    path('<slug:slug>/success/', views.SuccessExpense.as_view(), name='success'),
 ]
