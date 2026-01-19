ff15586f386e1121a08269da1f44efb8f0165995
rdnff11 <166251630+rdnff11@users.noreply.github.com>
2026-01-19 22:41:51 +0300

    Update forms.py

--- geely_site/geely/forms.py
+++ geely_site/geely/forms.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+from django.utils import timezone
+
 from django import forms
 from .models import Category
 
@@ -5,6 +9,8 @@ from .models import Expense
 
 
 class AddExpenseForm(forms.ModelForm):
+    date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), label='ДАТА')
+    # mileage = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'НЕ УКАЗАН'}))
     class Meta:
         model = Expense
         fields = ['date', 'mileage', 'price']
